2319ec206785b55f6f9a492098564532e3c860f6
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-06-17 01:32:12 -0400

    Write home page and fix images

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomePageComponent,
+    ProjectsPageComponent,
+    AboutPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [
     HeaderComponent,
