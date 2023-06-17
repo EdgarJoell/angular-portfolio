af1126707d3e139f6b92a2dc1331af21becf3813
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-06-17 01:32:31 -0400

    Fix images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'projects', component: ProjectsPageComponent },
+  { path: 'about', component: AboutPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
