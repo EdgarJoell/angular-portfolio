67d4c02a9884c8a77c1ebab5c6adc524ab1cb767
Edgar Zambrana <edgarzambrana@Edgars-Mac-mini.local>
2024-02-06 23:19:07 -0500

    Fix imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
+import { SharedModule } from './components/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ComponentsModule } from './components/components.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule
+    ComponentsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
