ca72b7bb5fe7b9c32a8501f885c6edae57e8227f
Edgar Zambrana <edgarzambrana@Edgars-Mac-mini.local>
2024-02-06 23:12:22 -0500

    Add shared folder and update imports

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,31 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { AppRoutingModule } from '../app-routing.module';
-import { DropdownDirective } from './header/dropdown.directive';
+import { SharedModule } from './shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent,
     HomePageComponent,
     ProjectsPageComponent,
     AboutPageComponent,
-    DropdownDirective
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   exports: [
-    HeaderComponent,
-    FooterComponent
+    HomePageComponent,
+    ProjectsPageComponent,
+    AboutPageComponent,
+    SharedModule
   ]
 })
 export class ComponentsModule { }
