17b2d01ac2aa1916c8ce03382a3153071c651370
Edgar Zambrana <edgarjoel0920@gmail.com>
2024-02-20 22:14:07 -0500

    Add singular project route. Update Project interface and update projects array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
+import { ProjectComponent } from './components/projects-page/project/project.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'projects', component: ProjectsPageComponent },
-  { path: 'about', component: AboutPageComponent }
+  { path: 'about', component: AboutPageComponent },
+  { path: 'projects/:id', component: ProjectComponent}
 ];
 
 @NgModule({
